a69973e9d367f20136fdc8148be895cc38e5d150
infraction <chris.anton.94@gmail.com>
2022-12-02 00:23:50 +0000

    added second part of challenge in new function

--- main.py
+++ main.py
@@ -14,4 +14,19 @@ def Find_Highest_Calorie(file):
             check_sum += int(line)
     return highest_sum
 
-print(Find_Highest_Calorie(test))
+def Find_Top_3(file):
+    highest_sums = [0,0,0]
+    check_sum = 0
+
+    for line in file:
+        if line.startswith('\n'):
+            if check_sum > highest_sums[0]:
+                highest_sums[0] = check_sum
+                highest_sums.sort()
+            check_sum = 0
+        else:
+            check_sum += int(line)
+    top3 = highest_sums[0]+highest_sums[1]+highest_sums[2]
+    return top3
+
+print(Find_Top_3(test))
