d562d2bf3e4ad9ee2a21cb9c684c82ec078a7200
infraction <chris.anton.94@gmail.com>
2022-12-02 00:14:52 +0000

    missed reseting check sum everytime

--- main.py
+++ main.py
@@ -7,15 +7,11 @@ def Find_Highest_Calorie(file):
     check_sum = 0
     for line in file:
         if line.startswith('\n'):
-            print(line)
             if check_sum > highest_sum:
                 highest_sum = check_sum
-            else:
-                print(line)
-                check_sum = 0
+            check_sum = 0
         else:
             check_sum += int(line)
-        x = input()
     return highest_sum
 
 print(Find_Highest_Calorie(test))
